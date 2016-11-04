8c0fd7225e9a53f07e1129007e3d3c3a2b81cf25
Dmitrii Donetskii <don.skoy.333@gmail.com>
2016-11-04 17:02:06 +0300

    Django 1.10 compatibility

--- new1303ru/urls.py
+++ new1303ru/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from home.views import home
 
 urlpatterns = [
     # Examples:
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', 'home.views.home', name='home'),
+    url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 ]
