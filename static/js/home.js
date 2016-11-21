e6b2c2e275a18e63957337c794a42ba587c7c416
Dmitrii Donetskii <donetkiydmitriy@gmail.com>
2016-11-21 21:50:18 +0300

    Homepage header

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,12 @@
-$( document ).ready(function(){
+$(document).ready(function () {
     $(".button-collapse").sideNav();
+    $('.parallax').parallax();
+    initHeader();
 });
+
+function initHeader() {
+    var sidepadding = ($("#header-container").width() - $("#header").width()) / 2;
+
+    $("#header").css('margin-left', sidepadding);
+    $("#header").css('margin-right', sidepadding);
+}
