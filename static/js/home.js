7cd2b3d321cb4cdb1a673b33f787662ce86f69df
Dmitrii Donetskii <don.skoy.333@gmail.com>
2016-11-21 22:29:09 +0300

    Sticky navigation

--- static/js/home.js
+++ static/js/home.js
@@ -1,7 +1,12 @@
+var navTopOffset;
+var navWrapper;
+
 $(document).ready(function () {
     $(".button-collapse").sideNav();
     $('.parallax').parallax();
+    navWrapper = $(".nav-wrapper");
     initHeader();
+    initStickyNavigation();
 });
 
 function initHeader() {
@@ -9,4 +14,38 @@ function initHeader() {
 
     $("#header").css('margin-left', sidepadding);
     $("#header").css('margin-right', sidepadding);
+}
+
+function initStickyNavigation() {
+    navTopOffset = $('nav').offset().top;
+    syncStickiness();
+    $(window).scroll(function () {
+        syncStickiness();
+    });
+}
+
+function syncStickiness() {
+    var scrollTopOffset = $(window).scrollTop();
+    if (scrollTopOffset > navTopOffset) {
+        $('nav').addClass('sticky');
+    } else {
+        $('nav').removeClass('sticky');
+    }
+}
+
+// Типо что-то красивое сделать хотел, но не получилось))00)
+function makeNavTransparent() {
+    if (navWrapper !== undefined) {
+        navWrapper.addClass('white');
+        navWrapper.addClass('black-text');
+        navWrapper.removeClass('red');
+    }
+}
+
+function makeNavRed() {
+    if (navWrapper !== undefined) {
+        navWrapper.removeClass('white');
+        navWrapper.removeClass('black-text');
+        navWrapper.addClass('red');
+    }
 }
